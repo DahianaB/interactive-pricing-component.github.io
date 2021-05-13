b6c50fbd492235f26258e644f87ee25372d05db6
Dahiana <nanaandrea2215@gmail.com>
2021-05-13 07:28:48 -0500

    Commit

---
+++ card-code.js
@@ -0,0 +1,11 @@
+const slider = document.querySelector(".slider");
+const  txt3 = document.querySelector(".txt3");
+const txt5  = document.querySelector(".text5");
+let swith = document.querySelector(".swith");
+
+// slider animado
+slider.addEventListener{("input").function(){
+
+    let x = slider.Value;
+    let color = "linear-gradient(90deg, rgb(184, 250, 243)" + x + "hsl(224,65%, 95%,)" + x + "sl)";
+}};
