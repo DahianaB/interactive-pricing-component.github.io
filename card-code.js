52a6d6737421257d55d3433b50a5e7af996c5253
Dahiana <nanaandrea2215@gmail.com>
2021-05-14 07:53:49 -0500

    commit 2

--- card-code.js
+++ card-code.js
@@ -1,11 +1,57 @@
-const slider = document.querySelector(".slider");
-const  txt3 = document.querySelector(".txt3");
-const txt5  = document.querySelector(".text5");
-let swith = document.querySelector(".swith");
+const slider = document.querySelector("#slider");
+const  pageViews = document.querySelector("#pageviews");
+const price  = document.querySelector("#price");
+let promoSwitch = document.querySelector("#promo-switch");
+
+// vistas y precios
+function printPageViews(){
+    if(slider.value == "0"){
+        pageViews.textContent = "10k"
+        price.textContent = "8"
+    }else if(slider.value == "25"){
+        pageViews.textContent = "50k"
+        price.textContent = "12"
+    }else if(slider.value == "50"){
+        pageViews.textContent = "100k"
+        price.textContent = "16"
+    }else if(slider.value == "75"){
+        pageViews.textContent = "500k"
+        price.textContent = "24"
+    }else if(slider.value == "100"){
+        pageViews.textContent = "1M"
+        price.textContent = "36"
+    }
+}
 
 // slider animado
-slider.addEventListener{("input").function(){
 
-    let x = slider.Value;
-    let color = "linear-gradient(90deg, rgb(184, 250, 243)" + x + "hsl(224,65%, 95%,)" + x + "sl)";
-}};
+slider.addEventListener("input",function (){
+    let x = slider.value;
+    //let color = "linear-gradient(90deg, rgb(184, 250, 243)" + x + "%, silver" + x + "%)";
+    let color = `linear-gradient(90deg, rgb(184, 250, 243) ${x}%, silver ${x}%)`;
+    slider.style.background = color;
+
+    printPageViews ();
+    
+    if (promoSwitch.checked == true){
+        let x = price.textContent*0.25;
+        price.textContent = price.textContent - x;
+        console.log(x);
+    }else{
+        printPageViews ();
+    };
+});
+
+
+// descuento ckeckbo
+promoSwitch.addEventListener("input",function(){
+  if(promoSwitch.checked == true){
+     
+    let x = price.textContent*0.25;
+    price.textContent =price.textContent - x; 
+  }else{
+      printPageViews();
+  }
+
+});
+    
